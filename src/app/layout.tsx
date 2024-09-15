1293b67683fc673d1c39b2e1ae39a6890dc1e1a4
Novc <108072504+C4sa@users.noreply.github.com>
2024-09-15 08:47:29 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
 			as="html" lang="en"
 			fillHeight background="page"
 			//data-neutral="gray" data-brand="blue" data-accent="violet"
-			data-solid="color" data-solid-style="flat"
+			//data-solid="color" data-solid-style="flat"
 			data-theme="dark"
 			data-brand="magenta"
 			data-accent="orange"
